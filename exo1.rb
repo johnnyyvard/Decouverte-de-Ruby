18f89d4c08feaf656f106843b799e94a5f3b9fc2
johnnyyvard <johnnyyvard@me.com>
2021-01-05 16:28:59 +0100

    Les conditions

--- exo1.rb
+++ exo1.rb
@@ -1,3 +1,18 @@
-puts "Entrez un chiffre"
-chiffre = gets.chomp.to_i
-puts chiffre.even?
+# puts "Entrez un chiffre"
+# chiffre = gets.chomp.to_i
+# if !chiffre.even?
+#     puts "Ton chiffre est impair !"
+# end
+
+#Demander un mot à l'utilisateur
+puts "Entrez un mot"
+#Stocker le mot dans une variable
+mot = gets.chomp.downcase
+#Créer une nouvelle variable inversé
+# mot_inverse = mot.reverse
+#Vérifier si le mot inversé == mot tapé
+if mot == mot.reverse
+    puts "C'est un palyndrome"
+else
+    puts "Ce n'est pas un palyndrome"
+end
